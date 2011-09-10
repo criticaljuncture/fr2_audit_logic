8822dbe2304bf0a49cebe70964db5dc025f6e318
Andrew Carpenter <acarpen@wested.org>
2011-09-09 17:05:12 -0700

    calculate problem_counts_by_year

--- lib/audit.rb
+++ lib/audit.rb
@@ -5,10 +5,28 @@ class Audit
   field :completed_at,    :type => DateTime
   field :options,         :type => Hash
   field :type,            :type => String
+  field :problem_counts_by_year,  :type => Hash
 
   embeds_many :audit_problems, :class_name => "AuditProblem::Base"
 
+  before_save :calculate_problem_counts_by_year
+
   def self.using_object_ids?
     false
+  end
+
+  private
+
+  def calculate_problem_counts_by_year
+    counts = {}
+    (1994..Date.current.year).each do |year|
+      counts[year] = 0
+    end
+
+    audit_problems.map(&:publication_date).each do |date| 
+      counts[date.year] += 1
+    end
+
+    self.problem_counts_by_year = counts
   end
 end
